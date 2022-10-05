ab45cfbdb6a265f7f76e288608f00bc60ddab286
evarghese563 <54088828+evarghese563@users.noreply.github.com>
2022-10-04 20:18:58 -0700

    Update GhibliRepository.java

--- Ghibli/src/main/java/com/Movie/Ghibli/Data/GhibliRepository.java
+++ Ghibli/src/main/java/com/Movie/Ghibli/Data/GhibliRepository.java
@@ -2,8 +2,6 @@ package com.Movie.Ghibli.Data;
 
 import java.util.List;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.Movie.Ghibli.GhibObject.Ghibli;
@@ -17,5 +15,5 @@ public interface GhibliRepository extends CrudRepository<Ghibli, Long> {
 
     List<Ghibli> findAll();
 
-    // List<Ghibli> existsById(int id);
+   
 }
